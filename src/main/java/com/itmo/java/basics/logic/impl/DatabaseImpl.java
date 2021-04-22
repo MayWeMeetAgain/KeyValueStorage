99f644e7e261a2864691e42338692e0c16fe0353
MayWeMeetAgain <79799393+MayWeMeetAgain@users.noreply.github.com>
2021-04-22 23:33:36 +0300

    Lab1 (#1)
    * Lab1 created
    
    * code style improved
    
    * deleting logic changed
    
    * code style improved
    
    * gitignore file changed
    
    * Delete .classpath
    
    * Delete .project
    
    * Delete .settings directory

--- src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
+++ src/main/java/com/itmo/java/basics/logic/impl/DatabaseImpl.java
@@ -1,38 +1,81 @@
 package com.itmo.java.basics.logic.impl;
 
 import com.itmo.java.basics.exceptions.DatabaseException;
+import com.itmo.java.basics.index.impl.TableIndex;
 import com.itmo.java.basics.logic.Database;
+import com.itmo.java.basics.logic.Table;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class DatabaseImpl implements Database {
+    private final String dbName;
+    private final Path dbRoot;
+    private final Map<String, Table> tables;
+
+    private DatabaseImpl (String dbName, Path dbRoot) {
+        this.dbName = dbName;
+        this.dbRoot = dbRoot;
+        tables = new HashMap<String, Table>();
+    }
+
     public static Database create(String dbName, Path databaseRoot) throws DatabaseException {
-        return null;
+        if (dbName.isEmpty() || dbName == null) {
+            throw new DatabaseException("Given database name is empty or null");
+        }
+        try {
+            Files.createDirectory(Paths.get(databaseRoot.toString(), dbName));
+        } catch (FileAlreadyExistsException e) {
+            throw new DatabaseException(String.format("Database with name %s at path %s already exist", dbName, databaseRoot.toString()), e);
+        } catch (IOException e) {
+            throw new DatabaseException(String.format("IO Exception while creating database %s at path %s", dbName, databaseRoot.toString()), e);
+        }
+        
+        return new DatabaseImpl(dbName, databaseRoot);
     }
 
     @Override
     public String getName() {
-        return null;
+        return dbName;
     }
 
     @Override
     public void createTableIfNotExists(String tableName) throws DatabaseException {
-
+        if (tableName.isEmpty() || tableName == null ) {
+            throw new DatabaseException("Given table name is empty or null");   
+        }
+        Table curTable = TableImpl.create(tableName, Paths.get(dbRoot.toString(), dbName), new TableIndex());
+        tables.put(tableName, curTable);
     }
 
     @Override
     public void write(String tableName, String objectKey, byte[] objectValue) throws DatabaseException {
-
+        searchTable(tableName).write(objectKey, objectValue);  
     }
 
     @Override
     public Optional<byte[]> read(String tableName, String objectKey) throws DatabaseException {
-        return Optional.empty();
+        return searchTable(tableName).read(objectKey);
     }
 
     @Override
     public void delete(String tableName, String objectKey) throws DatabaseException {
-
+        searchTable(tableName).delete(objectKey);
     }
+
+    private Table searchTable(String tableName) throws DatabaseException {
+        Table requiredTable = tables.get(tableName);
+        
+        if (requiredTable == null) {
+            throw new DatabaseException("Database doesn't have table with name" + tableName);
+        }
+        return requiredTable;
+    }
+
 }
--- src/main/java/com/itmo/java/basics/logic/io/DatabaseInputStream.java
+++ src/main/java/com/itmo/java/basics/logic/io/DatabaseInputStream.java
@@ -2,8 +2,10 @@ package com.itmo.java.basics.logic.io;
 
 import com.itmo.java.basics.logic.DatabaseRecord;
 import com.itmo.java.basics.logic.WritableDatabaseRecord;
+import com.itmo.java.basics.logic.impl.SetDatabaseRecord;
 
 import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
@@ -12,7 +14,7 @@ import java.util.Optional;
  * Класс, отвечающий за чтение данных из БД
  */
 public class DatabaseInputStream extends DataInputStream {
-    private static final int REMOVED_OBJECT_SIZE = -1;
+    private static final int SIZE_OF_REMOVED = -1;
 
     public DatabaseInputStream(InputStream inputStream) {
         super(inputStream);
@@ -23,6 +25,19 @@ public class DatabaseInputStream extends DataInputStream {
      * @return следующую запись, если она существует. {@link Optional#empty()} - если конец файла достигнут
      */
     public Optional<DatabaseRecord> readDbUnit() throws IOException {
-        return null;
+        try {
+            byte[] key = new byte[readInt()];
+            read(key);
+            int valueSize = readInt();
+            if (valueSize == SIZE_OF_REMOVED) {
+                return Optional.empty();
+            }
+            byte[] value = new byte[valueSize];
+            read(value);
+            WritableDatabaseRecord wRecord = new SetDatabaseRecord(key, value); 
+            return Optional.of(wRecord);
+        } catch (EOFException e) {
+            return Optional.empty();
+        }
     }
 }
